f4404f654a9b105bcbf6ac35bfbedef0111a7b3d
madja0125 <dejesusmiguel788@gmail.com>
2024-04-26 17:29:30 +0000

    adding number 1

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Module that concatenate two variables that are strings"""
+
+
+def concat(a: str, b: str) -> str:
+    """function that concatenates two variables"""
+    str = a + b
+    return str
