45d6e708277f47854d665d83db36c917d9c76c31
madja0125 <dejesusmiguel788@gmail.com>
2024-05-11 02:13:27 +0000

    fixing 7

--- ES6_basic/7-getBudgetObject.js
+++ ES6_basic/7-getBudgetObject.js
@@ -1,10 +1,7 @@
-/* eslint-disable */
 export default function getBudgetObject(income, gdp, capita) {
-    const budget = {
-        income,
-        gdp,
-        capita,
-    };
-
-    return budget;
+  return {
+    income,
+    gdp,
+    capita,
+  };
 }
