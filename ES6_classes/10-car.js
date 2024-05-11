c5f5659d549893202e7410329339fa49dd51ebb0
madja0125 <dejesusmiguel788@gmail.com>
2024-05-11 02:29:25 +0000

    adding 10

---
+++ ES6_classes/10-car.js
@@ -0,0 +1,16 @@
+/* eslint-disable */
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  static get [Symbol.species]() {
+    return this;
+  }
+
+  cloneCar() {
+    return new this.constructor(this._brand, this._motor, this._color);
+  }
+}
