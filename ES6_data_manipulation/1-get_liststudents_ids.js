fa5ad1be6eacbbf059aca27f6d9b39eb76fec7ad
madja0125 <dejesusmiguel788@gmail.com>
2024-05-24 13:11:27 +0000

    adding 1

---
+++ ES6_data_manipulation/1-get_liststudents_ids.js
@@ -0,0 +1,10 @@
+/* eslint-disable */
+export default function getStudentsByLocation(students, city) {
+    if (!Array.isArray(students)) {
+      return [];
+    }
+  
+    const filter = students.filter((filteredObj) => filteredObj.location === city);
+    return filter;
+  }
+
